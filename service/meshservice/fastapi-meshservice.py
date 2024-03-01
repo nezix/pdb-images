2e111b26977e85681ce3aacc6c4d201d2e2d1f0e
Xavier Martinez <xavier@nanome.ai>
2024-03-01 22:15:42 +0100

    Add local file entry point

--- service/meshservice/fastapi-meshservice.py
+++ service/meshservice/fastapi-meshservice.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 import tempfile
+import shutil
 
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.gzip import GZipMiddleware
 
 to_clean = []
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
+
 
 def clean():
     global to_clean
@@ -17,12 +19,38 @@ def clean():
         temp.cleanup()
     to_clean = []
 
+
 class PDBImagesMesh():
-    def run_pdb_images_pdbid(self, pdbId: str, output_folder: str, arguments: dict= {}):
+    def run_pdb_images_pdbid(self, pdbId: str, output_folder: str, arguments: dict = {}):
         if not len(arguments):
-            arguments ={"repMode": "mesh", "showHydrogens":False, "showBranchedSticks":True, "ensembleShades":False, "forceBfactor":False}
-            
-        args = ["xvfb-run", "--auto-servernum", "pdb-images", pdbId, output_folder, "--type", arguments["repMode"]]
+            arguments = {"repMode": "mesh", "showHydrogens": False,
+                         "showBranchedSticks": True, "ensembleShades": False, "forceBfactor": False}
+
+        args = ["xvfb-run", "--auto-servernum", "pdb-images",
+                pdbId, output_folder, "--type", arguments["repMode"]]
+        if "showHydrogens" in arguments and arguments["showHydrogens"]:
+            args += ["--show-hydrogens"]
+        if "showBranchedSticks" in arguments and arguments["showBranchedSticks"]:
+            args += ["--show-branched-sticks"]
+        if "ensembleShades" in arguments and arguments["ensembleShades"]:
+            args += ["--ensemble-shades"]
+        if "forceBfactor" in arguments and arguments["forceBfactor"]:
+            args += ["--force-bfactor"]
+
+        print(f"Starting with args: {args}")
+
+        result = subprocess.run(args, capture_output=True)
+        if len(result.stderr) > 0:
+            return result.stderr.decode("utf-8")
+        return ""
+
+    def run_pdb_images_file(self, cif_path: str, output_folder: str, arguments: dict = {}):
+        if not len(arguments):
+            arguments = {"repMode": "mesh", "showHydrogens": False,
+                         "showBranchedSticks": True, "ensembleShades": False, "forceBfactor": False}
+
+        args = ["xvfb-run", "--auto-servernum", "pdb-images", "--input",
+                cif_path, "XXXX", output_folder, "--type", arguments["repMode"]]
         if "showHydrogens" in arguments and arguments["showHydrogens"]:
             args += ["--show-hydrogens"]
         if "showBranchedSticks" in arguments and arguments["showBranchedSticks"]:
@@ -43,20 +71,60 @@ class PDBImagesMesh():
         global to_clean
         print("Run pdb-images for " + pdb_id)
         tempdir_out = tempfile.TemporaryDirectory(prefix="pdb_images_output_")
-        ret = self.run_pdb_images_pdbid(pdb_id.lower(), tempdir_out.name, arguments)
+        ret = self.run_pdb_images_pdbid(
+            pdb_id.lower(), tempdir_out.name, arguments)
         if ret:
-            raise Exception(f"Something went wrong when executing pdb-images: {ret}")
+            raise Exception(
+                f"Something went wrong when executing pdb-images: {ret}")
 
-        mesh_files = [filename for filename in os.listdir(tempdir_out.name) if filename.endswith(".usdz")]
+        mesh_files = [filename for filename in os.listdir(
+            tempdir_out.name) if filename.endswith(".usdz")]
         if len(mesh_files) == 0:
-            raise Exception(f"Something went wrong when executing pdb-images, no usdz file written")
-        
+            raise Exception(
+                f"Something went wrong when executing pdb-images, no usdz file written")
+
+        to_clean.append(tempdir_out)
+        return FileResponse(path=os.path.join(tempdir_out.name, mesh_files[0]), filename=mesh_files[0])
+
+    async def GetMeshLocal(self, file_path: str, arguments: dict = {}):
+        global to_clean
+        tempdir_out = tempfile.TemporaryDirectory(prefix="pdb_images_output_")
+        print("Run pdb-images for " + file_path)
+        to_clean.append(tempdir_out)
+        shutil.move(file_path, tempdir_out.name)
+        cif_path = os.path.join(tempdir_out.name, os.path.split(file_path)[1])
+
+        ret = self.run_pdb_images_file(cif_path, tempdir_out.name, arguments)
+        if ret:
+            raise Exception(
+                f"Something went wrong when executing pdb-images: {ret}")
+
+        mesh_files = [filename for filename in os.listdir(
+            tempdir_out.name) if filename.endswith(".usdz")]
+        if len(mesh_files) == 0:
+            raise Exception(
+                f"Something went wrong when executing pdb-images, no usdz file written")
+
         to_clean.append(tempdir_out)
         return FileResponse(path=os.path.join(tempdir_out.name, mesh_files[0]), filename=mesh_files[0])
 
 
 @app.get("/getmesh/{pdb_id}")
-async def read_item(pdb_id: str, background_tasks: BackgroundTasks):
+async def getmesh_pdbid(pdb_id: str, background_tasks: BackgroundTasks):
     background_tasks.add_task(clean)
     pdbimages = PDBImagesMesh()
     return await pdbimages.GetMesh(pdb_id)
+
+
+@app.post("/getmesh-file/")
+async def getmesh_file(cif_file: UploadFile = File(...),
+                       background_tasks: BackgroundTasks = BackgroundTasks()):
+    background_tasks.add_task(clean)
+    exts = [".cif", ".bcif", ".cif.gz", ".bcif.gz"]
+    if not any(cif_file.filename.endswith(s) for s in exts):
+        raise Exception(f"Only {exts} file extensions are accepted")
+    cif_path = "/tmp/" + cif_file.filename
+    with open(cif_path, "wb") as f:
+        shutil.copyfileobj(cif_file.file, f)
+    pdbimages = PDBImagesMesh()
+    return await pdbimages.GetMeshLocal(cif_path)
